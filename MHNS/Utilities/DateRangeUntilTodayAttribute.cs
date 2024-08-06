05a14629974b4e59a7252b8f6e0e8ee2fb76e402
roel <sungaroelbimbo24@gmail.com>
2024-08-06 12:06:13 +0800

    test03

--- MHNS/Utilities/DateRangeUntilTodayAttribute.cs
+++ MHNS/Utilities/DateRangeUntilTodayAttribute.cs
@@ -1,6 +1,27 @@
-﻿namespace MHNS.Utilities
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace MHNS.Utilities
 {
-    public class DateRangeUntilTodayAttribute
+    public class DateRangeUntilTodayAttribute : ValidationAttribute
     {
+        private readonly DateTime _minDate;
+
+        public DateRangeUntilTodayAttribute(string minDate)
+        {
+            _minDate = DateTime.Parse(minDate);
+        }
+
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            if (value is DateTime date)
+            {
+                if (date < _minDate || date > DateTime.Now)
+                {
+                    return new ValidationResult($"Birthdate must be between {_minDate.ToShortDateString()} and {DateTime.Now.ToShortDateString()}");
+                }
+            }
+            return ValidationResult.Success;
+        }
     }
 }
--- MHNS/obj/Debug/net8.0/MHNS.AssemblyInfo.cs
+++ MHNS/obj/Debug/net8.0/MHNS.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MHNS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9cd97929769951776689cbffa8982134f1bdc157")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8bc6e4cdbd565aba9bf6107707af66d8f44fac8f")]
 [assembly: System.Reflection.AssemblyProductAttribute("MHNS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MHNS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
